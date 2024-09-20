5d6002e865a46d28ed96a76cb679fa689ebace8d
ilya <ponoia@yandex.ru>
2024-09-20 10:59:30 +0300

    flags

---
+++ flag.py
@@ -0,0 +1,12 @@
+print('\x1b[48;5;1m                     \x1b[0m')
+
+print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+
+print('\x1b[48;5;1m   \x1b[48;5;7m              \x1b[48;5;1m    \x1b[0m')
+print('\x1b[48;5;1m   \x1b[48;5;7m              \x1b[48;5;1m    \x1b[0m')
+
+print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+
+print('\x1b[48;5;1m                     \x1b[0m')
