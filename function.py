8442fe82dd6603ca4b36a74072cdec350357d95c
ilya <ponoia@yandex.ru>
2024-10-08 18:50:28 +0300

    function_update

--- function.py
+++ function.py
@@ -1,9 +1,7 @@
-print('                                                        \x1b[48;5;7m       \x1b[0m')
-print('                                                 \x1b[48;5;7m       \x1b[0m')
-print('                                          \x1b[48;5;7m       \x1b[0m')
-print('                                   \x1b[48;5;7m       \x1b[0m')
-print('                            \x1b[48;5;7m       \x1b[0m')
-print('                     \x1b[48;5;7m       \x1b[0m')
-print('              \x1b[48;5;7m       \x1b[0m')
-print('       \x1b[48;5;7m       \x1b[0m')
-print('\x1b[48;5;7m       \x1b[0m')
+def print_line(offset):
+    print(f'{' ' * offset}\x1b[48;5;7m{' ' * 5}\x1b[0m')
+
+for y in range(11, 0, -1):
+    print_line(y*3)
+
+    
