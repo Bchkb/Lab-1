aeeda110da54f7ac31b3d7510540bc62f27cb92e
ilya <ponoia@yandex.ru>
2024-10-08 20:12:18 +0300

    uzor_update

--- uzor.py
+++ uzor.py
@@ -1,18 +1,36 @@
-import os
 import time
 
-offset = ' '
-line = ' '
-colors = [7, 3, 4]
-while True:
-    for color in colors:
-        print(f'{offset * 8}\x1b[48;5;{color}m{line * 8}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 8}\x1b[0m')
-        print(f'{offset * 6}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m')
-        print(f'{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m')
-        print(f'{offset * 2}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 0}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 4}\x1b[0m')
-        print(f'{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m')
-        print(f'{offset * 6}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m')
-        print(f'{offset * 8}\x1b[48;5;{color}m{line * 8}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 8}\x1b[0m')
+offset = 12
+line = 4
 
-        time.sleep(1)
-        os.system("cls")
+WHITE = 7
+YELLOW = 3
+BLUE = 4
+colors = [WHITE,  YELLOW, BLUE]
+
+def print_line(offset, long, color):
+    return f'{offset * ' '}\x1b[48;5;{color}m{' ' * long}\x1b[{offset//2 * ' '}C\x1b[0m'
+print(print_line(offset, line, WHITE) + print_line(offset, line, WHITE))
+print(print_line(offset - 4, line, WHITE) + print_line(offset - 4, line, WHITE))
+
+# while True:
+#     for color in colors:
+        # print(f'{offset * 8}\x1b[48;5;{color}m{line * 8}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 8}\x1b[0m')
+        # print(f'{offset * 6}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m')
+        # print(f'{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m')
+        # print(f'{offset * 2}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 0}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 4}\x1b[0m')
+        # print(f'{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m')
+        # print(f'{offset * 6}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m{offset * 8}\x1b[48;5;{color}m{line * 4}\x1b[0m\x1b[0m{offset * 4}\x1b[48;5;{color}m{line * 4}\x1b[0m')
+        # print(f'{offset * 8}\x1b[48;5;{color}m{line * 8}\x1b[0m{offset * 12}\x1b[48;5;{color}m{line * 8}\x1b[0m')
+        # for i in range(7):
+        #     print(print_line(offset, line, color) + print_line(offset, line, color))
+        #     if i <= 3:
+        #         offset -= 4
+        #     if i > 3:
+        #         offset += 4
+
+
+
+        # time.sleep(1)
+        # print('\x1b[8A')
+        
