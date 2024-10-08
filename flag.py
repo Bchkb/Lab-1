78b9e3d8df7ae8ab671fb10bcac3d9603b2a91a2
ilya <ponoia@yandex.ru>
2024-10-08 13:20:36 +0300

    flag_update

--- flag.py
+++ flag.py
@@ -1,12 +1,21 @@
-print('\x1b[48;5;1m                     \x1b[0m')
+RED = 1
+WHITE = 7
 
-print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
-print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+def square_print(long, color, offset):
+    print(f'{offset * ' '}\x1b[48;5;{color}m{long * ' '}\x1b[0m')
 
-print('\x1b[48;5;1m   \x1b[48;5;7m              \x1b[48;5;1m    \x1b[0m')
-print('\x1b[48;5;1m   \x1b[48;5;7m              \x1b[48;5;1m    \x1b[0m')
+def plus_print(color_offset, long, color, offset):
+    print(f'\x1b[48;5;{color_offset}m{offset * ' '}\x1b[48;5;{color}m{long * ' '}\x1b[0m')
 
-print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
-print('\x1b[48;5;1m       \x1b[48;5;7m      \x1b[48;5;1m        \x1b[0m')
+for i in range(8):
+    square_print(25, RED, 0)
 
-print('\x1b[48;5;1m                     \x1b[0m')
+print('\x1b[8A')
+
+for i in range(8):
+    if i == 0 or i == 1 or i == 5 or i == 6:
+        plus_print(RED, 6, WHITE, 9)
+    if i == 3 or i == 4:
+        plus_print(RED, 18, WHITE, 3)
+
+
