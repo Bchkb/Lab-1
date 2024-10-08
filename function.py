6ad817d6b826323532cf9604674e6fd6b50a1892
Ilya <Ponoia@yandex.ru>
2024-10-08 20:23:41 +0300

    function_update

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 def print_line(offset):
-    print(f'{' ' * offset}\x1b[48;5;7m{' ' * 5}\x1b[0m')
+    print(f'{" " * offset}\x1b[48;5;7m{" " * 5}\x1b[0m')
 
 for y in range(11, 0, -1):
     print_line(y*3)
